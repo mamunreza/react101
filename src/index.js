7c67903ea90f7e58360188acd960a8e6bbc43762
Mamun Reza <Muhammad.Reza@energia.ee>
2024-03-21 20:16:20 +0200

    Chapter 3 ends

--- src/index.js
+++ src/index.js
@@ -1,29 +1,8 @@
-// 1) Import the React and ReactDOM libraries
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import App from './App';
 
-// 2) Get a reference to the div with ID root
 const el = document.getElementById('root');
-
-// 3) Tell React to take control of that element
 const root = ReactDOM.createRoot(el);
 
-// 4) Create a component
-
-function App() {
-  return (
-    <div className="wrapper">
-      <textarea
-        readOnly
-        maxLength={3}
-        spellCheck
-        style={{ backgroundColor: 'gray' }}
-      />
-    </div>
-  );   
-}
-
-export default App;
-
-// 5) Show the component on the screen
 root.render(<App />);
