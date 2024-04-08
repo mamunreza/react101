7459f12e894521665a873bb2092d4eccee23e403
Mamun Reza <mamun.reza09@gmail.com>
2024-04-08 07:49:06 +0300

    ddl selected option in top label

--- src/App.js
+++ src/App.js
@@ -1,28 +1,22 @@
-import Accordion from './components/Accordion';
+import { useState } from 'react';
+import Dropdown from './components/Dropdown';
 
 function App() {
-  const items = [
-    {
-      id: 'l2kj5',
-      label: 'Can I use React on a project?',
-      content:
-        'You can use React on any project you want. You can use React on any project you want. You can use React on any project you want. You can use React on any project you want.',
-    },
-    {
-      id: 'lk2j35lkj',
-      label: 'Can I use Javascript on a project?',
-      content:
-        'You can use React on any project you want. You can use React on any project you want. You can use React on any project you want. You can use React on any project you want.',
-    },
-    {
-      id: 'l1kj2i0g',
-      label: 'Can I use CSS on a project?',
-      content:
-        'You can use React on any project you want. You can use React on any project you want. You can use React on any project you want. You can use React on any project you want.',
-    },
+  const [selection, setSelection] = useState(null);
+
+  const handleSelect = (option) => {
+    setSelection(option);
+  };
+
+  const options = [
+    { label: 'Red', value: 'red' },
+    { label: 'Green', value: 'green' },
+    { label: 'Blue', value: 'blue' },
   ];
 
-  return <Accordion items={items} />;
+  return (
+    <Dropdown options={options} selection={selection} onSelect={handleSelect} />
+  );
 }
 
 export default App;
