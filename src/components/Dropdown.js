f8440473b6a0fda1b52e72160c0dcfa565d76a7a
Mamun Reza <mamun.reza09@gmail.com>
2024-04-11 07:06:40 +0300

    useRef added

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,36 +1,56 @@
-import { useState } from 'react';
+import { useState, useEffect, useRef } from 'react';
+import { GoChevronDown } from 'react-icons/go';
+import Panel from './Panel';
 
-function Dropdown({ options, selection, onSelect }) {
+function Dropdown({ options, value, onChange }) {
   const [isOpen, setIsOpen] = useState(false);
+  const divEl = useRef();
+
+  useEffect(() => {
+    const handler = (event) => {
+      if (!divEl.current.contains(event.target)) {
+        setIsOpen(false);
+      }
+    };
+
+    document.addEventListener('click', handler, true);
+
+    return () => {
+      document.removeEventListener('click', handler);
+    };
+  }, []);
 
   const handleClick = () => {
     setIsOpen(!isOpen);
   };
 
   const handleOptionClick = (option) => {
-    // CLOSE DROPDOWN
     setIsOpen(false);
-    // WHAT OPTION DID THE USER CLICK ON???
-    onSelect(option);
+    onChange(option);
   };
 
   const renderedOptions = options.map((option) => {
     return (
-      <div onClick={() => handleOptionClick(option)} key={option.value}>
+      <div
+        className="hover:bg-sky-100 rounded cursor-pointer p-1"
+        onClick={() => handleOptionClick(option)}
+        key={option.value}
+      >
         {option.label}
       </div>
     );
   });
 
-  let content = 'Select...';
-  if (selection) {
-    content = selection.label;
-  }
-
   return (
-    <div>
-      <div onClick={handleClick}>{content}</div>
-      {isOpen && <div>{renderedOptions}</div>}
+    <div ref={divEl} className="w-48 relative">
+      <Panel
+        className="flex justify-between items-center cursor-pointer"
+        onClick={handleClick}
+      >
+        {value?.label || 'Select...'}
+        <GoChevronDown className="text-lg" />
+      </Panel>
+      {isOpen && <Panel className="absolute top-full">{renderedOptions}</Panel>}
     </div>
   );
 }
